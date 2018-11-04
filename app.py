9f2c26ad2ee2b38324de92a8075ab78227a7be78
Morgan Newman <mpnewman95@gmail.com>
2018-11-04 02:19:24 -0800

    Reverting back to working server state

--- app.py
+++ app.py
@@ -1,47 +1,27 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-
-#import request
+from flask_cors import CORS
 import json
 
+# spin up Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, send_wildcard=True)
 
-# @app.route('/json-example', methods=['POST'])  # GET requests will be blocked
-# def json_example():
-#     req_data = request.get_json()
-#
-#     language = req_data['language']
-#     language = None
-#     if 'language' in req_data:
-#         language = req_data['language']
-#     framework = req_data['framework']
-#     python_version = req_data['version_info']['python']  # two keys are needed because of the nested object
-#     example = req_data['examples'][0]  # an index is needed because of the array
-#     boolean_test = req_data['boolean_test']
-#
-#     return '''
-#            The language value is: {}
-#            The framework value is: {}
-#            The Python version is: {}
-#            The item at index 0 in the example list is: {}
-#            The boolean value is: {}'''
-
-
-
-
-@app.route('/home', methods=['GET', 'POST'])
+# POST /search endpoint
+@app.route('/search', methods=['POST'])
 def request_handler():
     if request.method == 'POST':
-        data = request.get_json()
+        data = (request.get_json())['content']
+        print data
         # HERE WE HAVE CONTENT TO PARSE
         # INSERT MAGIC FUNCTION TO DO MAGIC THINGS HERE
-        response = {'PLACEHOLDER': 'DATA'}
-        return jsonify(response), 201
+        response = jsonify({'PLACEHOLDER': 'DATA'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 201
+    else: 
+        return jsonify({'Error': 'does not exist'}, 404)
 
-    #return render_template(<template name e.g. xyz.html>, posts=data)
-
+# spin up server
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True, use_reloader=True)
